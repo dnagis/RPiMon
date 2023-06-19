446524effdb5cc6ff5cf5fd305f37cd4443aa454
dnagis <vincent.achard@gmail.com>
2023-06-19 16:47:01 +0200

    fin premiere version avant tests

--- bash/wifi_check.sh
+++ bash/wifi_check.sh
@@ -1,31 +1,14 @@
 #!/usr/bin/bash
 
+#Côté client: check des étapes de connexion wifi à partir de wpa_cli -i wlan0 status puis ping
+
+
+
 #collect info from wpa_cli -i wlan0 status
 #WPA_STATE=`wpa_cli -i wlan0 status | grep wpa_state | sed 's/wpa_state=//g'`
 #SSID=`wpa_cli -i wlan0 status | grep ^ssid | sed 's/ssid=//g'`
 #IP_ADDR=`wpa_cli -i wlan0 status | grep ip_address | sed 's/ip_address=//g'`
 
-#echo wpa_state=$WPA_STATE ssid=$SSID ip=$IP_ADDR
-
-#tests
-#if [ "$WPA_STATE" == "COMPLETED" ]; then
-#    echo "WPA_STATE=COMPLETED"
-#else
-#    echo "WPA_STATE=$WPA_STATE"
-#fi
-
-#if [ "$SSID" == "DIRECT-RPi4" ]; then
-#    echo "SSID=DIRECT-RPi4"
-#else
-#    echo "SSID=$SSID"
-#fi
-
-
-#if [ ! -z `echo $IP_ADDR | grep 192.168.49` ]; then
-#    echo "L'IP=$IP_ADDR contient bien 192.168.49"
-#else
-#    echo "L'IP=$IP_ADDR ne contient pas 192.168.49"
-#fi
 
 #boucle check WPA_STATE = COMPLETED
 while :
@@ -64,11 +47,15 @@ do
 	sleep 1
 done
 
-echo "on tente ping sur 192.168.49.1"
-ping -q -c 1 -W 1 192.168.49.1 2>&1 >/dev/null
-PING_RESP=$?
+#boucle check ping
+while :
+do
+ping -q -c 1 -W 1 192.168.49.1 2>&1 >/dev/null && break
+echo "ping 192.168.49.1 n'a pas retourné 0 on sleep 1s"
+sleep 1
+done
 
-echo "résultat du ping:$PING_RESP"
 
-echo "fin du script"
+
+echo -e "\\033[1;32mfin du script SSID=$SSID IP_ADDR=$IP_ADDR et ping OK\\033[0;39m"
 
