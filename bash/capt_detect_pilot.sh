8be042e39a26c139df990b99429221ef79fd05b5
dnagis <vincent.achard@gmail.com>
2023-06-25 12:33:13 +0200

    adaptation à un lancement depuis sysinit donc avec des paths absolues pas relatives

--- bash/capt_detect_pilot.sh
+++ bash/capt_detect_pilot.sh
@@ -2,18 +2,22 @@
 
 #Script qui controle le process de capture + detection
 
-#commandes a envoyer: echo start | socat - TCP:192.168.49.1:8001 
-#	start --> start en portrait (defaut)
+#commandes a envoyer: echo start po | socat - TCP:192.168.49.1:8001 
+#	start po --> start en portrait
 #	start pa --> start en paysage
 #	stop
 
 #sysinit:
 #socat -t 10 TCP-LISTEN:8001,fork EXEC:/bin/capt_detect_pilot.sh &
-#envoi pour tests depuis linux:
-#
+
 
 STDIN=$(cat) #Recuperation du message envoye par l emetteur
-read CMD ORIENTATION <<<$STDIN
+echo STDIN=$STDIN
+
+CMD=`echo $STDIN | cut -d' ' -f1`
+ORIENTATION=`echo $STDIN | cut -s -d' ' -f2` #sans -s: en labsence de delimiter cut renvoie la ligne entiere (donc le 1st field)
+
+
 echo script capt_detect_pilot.sh CMD=$CMD ORIENTATION=$ORIENTATION
 
 if [ "$ORIENTATION" == "pa" ]; then
@@ -30,8 +34,8 @@ PID_GST=`pidof gst-launch-1.0`
 case "$CMD" in
  start)
     [[ ! -z $PID_GST ]] && kill $PID_GST
-    logger start_rpimon
-    gst-launch-1.0 -e --quiet rpicamsrc ! video/x-raw,width=$WIDTH,height=$HEIGHT,format=BGR,framerate=30/1 ! tee name=t t. ! queue ! v4l2h264enc ! 'video/x-h264,level=(string)3' ! h264parse ! mp4mux ! filesink location=capture.mp4 t. ! queue ! fdsink | ./stdin_to_detect po 0.5 &
+    #logger start_rpimon
+    gst-launch-1.0 -e --quiet rpicamsrc ! video/x-raw,width=$WIDTH,height=$HEIGHT,format=BGR,framerate=30/1 ! tee name=t t. ! queue ! v4l2h264enc ! 'video/x-h264,level=(string)3' ! h264parse ! mp4mux ! filesink location=/root/capture.mp4 t. ! queue ! fdsink | /root/stdin_to_detect $ORIENTATION 0.5 &
     ;;
  stop)
     echo 'kill gst-launch-1.0 avec SIGINT' 
